a6ca4ec0e69c0c503cfe82b055eba706cc6b16dc
readmyface <sarthak.chakraborty0@gmail.com>
2021-07-13 16:30:48 +0530

    Firebase hosting, SSO, Register Modal UI, Homepage

--- src/firebase/firebase.sso.js
+++ src/firebase/firebase.sso.js
@@ -37,6 +37,80 @@ const googleSignIn = (successCallback) =>{
     });
 }
 
+const facebookSignIn = (successCallback) =>{
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().useDeviceLanguage();
+    firebase.auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+        db.collection("users").where("email", "==", result.user.email)
+        .get().then((querySnapshot) => {
+            if(querySnapshot['docs'].length>0){
+                loginRequestBundle(result.user.email,successCallback);
+            }
+            else{
+                const userObject = {
+                    city: "",
+                    email: result.user.email,
+                    f_name: result.user.displayName.split(" ")[0],
+                    l_name: result.user.displayName.split(" ")[1],
+                    mobile: "",
+                    roles: ["USER"]
+                }
+                createUserObject(userObject,successCallback)
+            }
+        })
+        .catch((error) => {
+            handleApiError(error);
+        });
+    }).catch((error) => {
+        handleApiError(error);
+    });
+}
+
+const twitterSignIn = (successCallback) =>{
+    var provider = new firebase.auth.TwitterAuthProvider();
+    firebase.auth().useDeviceLanguage();
+    firebase.auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+        db.collection("users").where("email", "==", result.user.email)
+        .get().then((querySnapshot) => {
+            if(querySnapshot['docs'].length>0){
+                loginRequestBundle(result.user.email,successCallback);
+            }
+            else{
+                const userObject = {
+                    city: "",
+                    email: result.user.email,
+                    f_name: result.user.displayName.split(" ")[0],
+                    l_name: result.user.displayName.split(" ")[1],
+                    mobile: "",
+                    roles: ["USER"]
+                }
+                createUserObject(userObject,successCallback)
+            }
+        })
+        .catch((error) => {
+            handleApiError(error);
+        });
+    }).catch((error) => {
+        handleApiError(error);
+    });
+}
+
+const appleSignIn = ()=>{
+    var error = {
+        message:'Apple Sign-in is not yet enabled!'
+    }
+    handleApiError(error);
+}
+
+
+
 export {
-    googleSignIn
+    googleSignIn,
+    facebookSignIn,
+    twitterSignIn,
+    appleSignIn
 }
--- src/pages/HomePage/Events/index.js
+++ src/pages/HomePage/Events/index.js
@@ -1,8 +1,38 @@
-import React from "react";
-import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBView, MDBContainer, MDBBtn } from
+import React ,{useState,useContext} from "react";
+import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBView, MDBContainer, MDBBtn , MDBModal } from
   "mdbreact";
+import firebase from "firebase/app";
+import Login from '../../../components/Login'
+import RegisterEvent from '../../RegisterEvent'
+import { AuthContext } from '../../../context/AuthContext';
 
 const Events = ({eventlist}) => {
+
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const [registerModalData, setRegisterModalData] = useState(null);
+  const [isCreateEventModalOpen, setIsCreateEventModalOpen] = useState(false);
+
+  const {user, setUser, setIsLoading, isLoading} = useContext(AuthContext);
+
+  const registerEvent = function(index){
+    const eventConsidered = eventlist[index];
+    setRegisterModalData(eventConsidered);
+    if(eventConsidered.is_reg_open){
+      const user = firebase.auth().currentUser;
+      if(user){
+        setIsCreateEventModalOpen(true);
+      }
+      else{
+        setIsLoginModalOpen(true);
+      }
+    }
+  }
+  const srcRender = function(big,small){
+    if(window.innerWidth>1000){
+      return big;
+    }
+    return small;
+  }
   return (
     <div className="parallax-section" id="events">
       <MDBContainer>
@@ -19,20 +49,31 @@ const Events = ({eventlist}) => {
           slide
         >
           <MDBCarouselInner>
-            {eventlist && eventlist.map(({name, poster_link_big}, index) => (
+            {eventlist && eventlist.map((event, index) => (
               <MDBCarouselItem itemId={index + 1}>
               <MDBView>
                 <img
+                  onClick={()=>registerEvent(index)}
+                  title="CLick to Register"
                   className="d-block w-100 img-fluid"
-                  src={poster_link_big}
-                  alt={name}
+                  src={srcRender(event.poster_link_big,event.poster_link_small)}
+                  alt={event.name}
                 />
+                {/* <button onClick={()=>registerEvent(event)}>
+                  <span>Click to register</span>
+                </button> */}
               </MDBView>
             </MDBCarouselItem>
             ))}
           </MDBCarouselInner>
         </MDBCarousel>
       </MDBContainer>
+      <MDBModal isOpen={isLoginModalOpen} centered>
+        <Login setUser={setUser} setIsLoading={setIsLoading} close={() => { setIsLoginModalOpen(false); }} />
+      </MDBModal>
+      <MDBModal id="register-event-modal" isOpen={isCreateEventModalOpen} centered>
+        <RegisterEvent userDetails={user} eventDetails={registerModalData} close={() => { setIsCreateEventModalOpen(false); }} />
+      </MDBModal>
     </div>
   );
 }
